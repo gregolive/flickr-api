fff0f0be77b83ce4eaa95f778ecde0b12c744ad2
Gregory Olive <gregoryolive@outlook.com>
2021-12-21 23:48:13 +0900

    Add home and results views and methods to StaticPages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,21 @@
+require 'flickr'
+
 class StaticPagesController < ApplicationController
+  before_action :set_flickr
+
   def home
 
   end
 
   def results
-    
+
+  end
+
+  private
+
+  def set_flickr
+    ENV['FLICKR_API_KEY']
+    ENV['FLICKR_SHARED_SECRET']
+    @flickr = Flickr.new
   end
 end
