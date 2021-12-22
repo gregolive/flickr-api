1ba50863add889fec4d7408e2bbd74dc95c8b5d9
Gregory Olive <gregoryolive@outlook.com>
2021-12-22 22:31:22 +0900

    Display images for user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,23 @@
 class StaticPagesController < ApplicationController
   before_action :set_flickr_api, only: %i[results]
 
+  ENV['FLICKR_API_KEY']
+  ENV['FLICKR_SHARED_SECRET']
+
   def home
   end
 
   def results
-    @user = @flickr.people.getInfo user_id: (params[:/][:user]) rescue nil
+    @query = params[:/][:user]
+    @user = @flickr.people.getInfo user_id: @query rescue nil
     if @user
-      #@photos = 
+      @photos = @flickr.people.getPhotos user_id: @query
     end
-    
   end
 
   private
 
   def set_flickr_api
-    ENV['FLICKR_API_KEY']
-    ENV['FLICKR_SHARED_SECRET']
     @flickr = Flickr.new
   end
 end
