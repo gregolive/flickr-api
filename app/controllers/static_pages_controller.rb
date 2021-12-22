e24a08f571639d190d3250b15fad228085a87dd6
Gregory Olive <gregoryolive@outlook.com>
2021-12-22 17:38:34 +0900

    Properly display user info on search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,20 @@
 class StaticPagesController < ApplicationController
+  before_action :set_flickr_api, only: %i[results]
+
   def home
   end
 
   def results
+    @user = @flickr.people.getInfo user_id: (params[:/][:user]) rescue nil
+    if @user
+      #@photos = 
+    end
+    
+  end
+
+  private
+
+  def set_flickr_api
     ENV['FLICKR_API_KEY']
     ENV['FLICKR_SHARED_SECRET']
     @flickr = Flickr.new
