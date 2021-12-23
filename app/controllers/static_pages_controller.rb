de88cf934f7184ba94debba44579127967d5fbf0
Gregory Olive <gregoryolive@outlook.com>
2021-12-24 01:35:15 +0900

    Remove error messages from controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,8 +10,8 @@ class StaticPagesController < ApplicationController
     begin
       flickr = Flickr.new
       @user = flickr.people.getInfo user_id: @query
-    rescue StandardError => e
-      flash[:alert] = "#{e.class} : #{e.message} Whoops! 🤭 No user found with ID: #{@query}"
+    rescue StandardError
+      flash[:alert] = "Whoops! 🤭 No user found with ID: #{@query}"
       redirect_to root_path
     else
       flash[:notice] = "Success! 🎉 Flickr user found with ID: #{@query}"
